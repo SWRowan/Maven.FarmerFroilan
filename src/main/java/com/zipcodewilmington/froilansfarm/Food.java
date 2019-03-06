a2f48d435a72d046ee3432c791def455d40e6d5b
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-06 06:54:37 -0500

    added toString method to Food in UML

--- src/main/java/com/zipcodewilmington/froilansfarm/Food.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Food.java
@@ -1,4 +1,8 @@
 package com.zipcodewilmington.froilansfarm;
 
 public abstract class Food implements Edible{
+    @Override
+    public String toString() {
+        return '\n' + this.getClass().getSimpleName() + "{}";
+    }
 }
