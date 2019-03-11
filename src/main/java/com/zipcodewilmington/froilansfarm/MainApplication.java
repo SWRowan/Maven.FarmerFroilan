e26c125888726dc276f62c481f25cfdf791661ab
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-03-10 20:48:31 -0400

    Commit Message

--- src/main/java/com/zipcodewilmington/froilansfarm/MainApplication.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/MainApplication.java
@@ -1,14 +1,15 @@
 package com.zipcodewilmington.froilansfarm;
 
 import com.zipcodewilmington.froilansfarm.containers.farm.Farm;
+import com.zipcodewilmington.froilansfarm.simulations.Friday;
 import com.zipcodewilmington.froilansfarm.simulations.Tuesday;
 
 public class MainApplication {
     public static void main(String[] args) {
         Farm farm = new Farm();
 //        farm.run();
-        Tuesday tuesday = new Tuesday();
-        tuesday.runSimulation(farm);
+        Friday friday = new Friday();
+        friday.runSimulation(farm);
 
     }
 
